a0e9182720ce8d620b2ad90effb0d55829d3cd6e
佐藤哲 <satouakira@satouakiranoMacBook.local>
2020-10-12 21:53:15 +0900

    mod JS 1012

---
+++ JS/main_1012.js
@@ -0,0 +1,9 @@
+  	"use strict";
+
+  	console.log('Hello World! from main_1012.js');
+
+  	const price = 1200;
+
+  	// 選択後、comm + D で同じ情報を選択できる
+  	console.log(price * 10);
+  	console.log(price * 15);
