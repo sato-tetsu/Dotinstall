b81b92e5e3e2a0adfa78828f39761d4d6111b3fa
佐藤哲 <satouakira@satouakiranoMacBook.local>
2020-10-12 22:00:40 +0900

    mod JS 1012

--- JS/main_1012.js
+++ JS/main_1012.js
@@ -2,8 +2,15 @@
 
   	console.log('Hello World! from main_1012.js');
 
-  	const price = 1200;
+  	// 定数 const - 後から変更不可
+  	// 変数 let - 後から変更可能
+  	let price = 1200;
 
   	// 選択後、comm + D で同じ情報を選択できる
+  	console.log(price * 10);
+  	console.log(price * 15);
+
+  	price = 1000;
+
   	console.log(price * 10);
   	console.log(price * 15);
