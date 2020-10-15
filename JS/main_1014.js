a11db2ed7378e30c269cc2f08425e3ed358f78f5
佐藤哲 <satouakira@satouakiranoMacBook.local>
2020-10-15 11:54:11 +0900

    add JS 1015

--- JS/main_1014.js
+++ JS/main_1014.js
@@ -1,10 +1,4 @@
   	"use strict";
 
-// const sum = (a,b,c) => a + b + c;
-
-// const total = sum(1, 2, 3) + sum(3, 4, 5);
-// console.log(total);
-
-const double = a => a * 2;
-
-console.log(double(12));
+const x = 100;
+console.log(x);
