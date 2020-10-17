03de8b4522e86cab65618da0ded7fe78b2b3cb17
vagrant <vagrant@localhost.localdomain>
2020-10-17 13:24:36 +0000

    add JS 1017

--- JS/main_1016.js
+++ JS/main_1016.js
@@ -1,21 +1,18 @@
   "use strict";
 
 {
-
-  const otherProps = {
-  	r: 4,
-  	color: "red",
-  };
-
   const point = {
   	x: 100,
   	y: 180,
-  	...otherProps,
   };
 
-  const {x, r, ...others} = point;
+  // const keys = Object.keys(point);
+  // keys.forEach(key => console.log(`Key: ${key} ${point[key]}`));
 
-  console.log(x);
-  console.log(r);
-  console.log(others);
+  const points = [
+    {x: 30, y: 20},
+    {x: 40, y: 15},
+    {x: 50, y: 10},
+  ];
+  console.log(points[1].y);
 }
