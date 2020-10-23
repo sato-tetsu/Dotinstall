b6d3e56f809d42574e0a205d3b6fc3a94e7d2c18
vagrant <vagrant@localhost.localdomain>
2020-10-23 12:59:05 +0000

    add JS 1023

--- JS/js/main_1022.js
+++ JS/js/main_1022.js
@@ -36,14 +36,35 @@
 
 	// 例外処理
 
-	const name = 5;
+	// const name = 5;
 
-	try{
-		console.log(name.toUpperCase());
-	}catch(e){
-		console.log(e);
-	}
-	
-	console.log("Finish");
+	// try{
+	// 	console.log(name.toUpperCase());
+	// }catch(e){
+	// 	console.log(e);
+	// }
+	// console.log("Finish");
+
+	class Post 
+
+	const posts = [
+		{
+			text: "勉強中",
+			likeCount: 0,
+			show: function(){
+				console.log(`${this.text} - ${this.likeCount}いいね`);
+			}
+		},{
+			text: "プログラミング",
+			likeCount: 0,
+			show(){
+				console.log(`${this.text} - ${this.likeCount}いいね`);
+			}
+		},
+	];
+
+	posts[0].show();
+	posts[1].show();
+
 
 }
