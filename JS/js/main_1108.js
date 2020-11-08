c5b0263353ca86c53ca4d463900e3aae6669739a
vagrant <vagrant@localhost.localdomain>
2020-11-08 07:27:23 +0000

    add 1107 JS

---
+++ JS/js/main_1108.js
@@ -0,0 +1,12 @@
+  "use strict";
+
+{
+  document.querySelector('button').addEventListener('click', () => {
+    const targetNode = document.getElementById("target");
+
+  	targetNode.textContent = "Changed!";
+    targetNode.title = "this is title!";
+    targetNode.style.color = "red";
+    targetNode.style.backgroundColor = "black";
+  });
+}
