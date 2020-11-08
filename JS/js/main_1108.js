4fb2c07229bbefb53805db65a1be4c795cefe320
vagrant <vagrant@localhost.localdomain>
2020-11-08 07:38:36 +0000

    mod 1107 JS

--- JS/js/main_1108.js
+++ JS/js/main_1108.js
@@ -4,9 +4,10 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById("target");
 
-  	targetNode.textContent = "Changed!";
-    targetNode.title = "this is title!";
-    targetNode.style.color = "red";
-    targetNode.style.backgroundColor = "black";
+  	// targetNode.textContent = "Changed!";
+    //  targetNode.title = "this is title!";
+    // targetNode.style.color = "red";
+    // targetNode.style.backgroundColor = "black";
+    targetNode.className = "my-color my-border";
   });
 }
