ad578387868af480e274229d3d98e6f9e8b81897
vagrant <vagrant@localhost.localdomain>
2020-11-10 14:32:42 +0000

    mod 1110 JS

--- JS/js/main_1110.js
+++ JS/js/main_1110.js
@@ -2,6 +2,10 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
+    const li = document.createElement("li");
+    const text = document.querySelector("input");
 
+    li.textContent = text.value;
+    document.querySelector("ul").appendChild(li);
   });
 }
