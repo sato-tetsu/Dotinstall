4cd845466c3998965339909178a60f719e6e3aeb
vagrant <vagrant@localhost.localdomain>
2020-11-18 14:35:06 +0000

    mod 1118 JS

--- JS/js/main_1118.js
+++ JS/js/main_1118.js
@@ -1,5 +1,22 @@
   "use strict";
 
 {
+  const text = document.querySelector("textarea");
+
+  document.addEventListener("focus", () => {
+    console.log("focus");
+  });
+  
+  document.addEventListener("blur", () => {
+    console.log("blur");
+  });
+  
+  document.addEventListener("input", () => {
+    console.log("inputing...");
+  });
+  
+  document.addEventListener("change", () => {
+    console.log(text.value.length);
+  });
   
 }
