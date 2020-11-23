53a05dcc7ceea4772bff15b05fcffa88d537329b
vagrant <vagrant@localhost.localdomain>
2020-11-23 22:49:54 +0000

    mod 1124 JS

--- JS/js/testApp_Bingo.js
+++ JS/js/testApp_Bingo.js
@@ -1,26 +1,14 @@
   "use strict";
 
 {
-  const btn = document.getElementById("btn");
+  const text = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const b = [];
 
-  btn.addEventListener("click", () => {
-  	const results = ["大吉", "吉", "末吉", "凶"];
-  	// const n = Math.floor(Math.random() * results.length);
-    // btn.textContent = "!!!";
-    // btn.textContent = results[n];
-    btn.textContent = results[Math.floor(Math.random() * results.length)];
+  b[0] = text.splice(Math.floor(Math.random() * text.length), 1)[0];
+  b[1] = text.splice(Math.floor(Math.random() * text.length), 1)[0];
+  b[2] = text.splice(Math.floor(Math.random() * text.length), 1)[0];
+  b[3] = text.splice(Math.floor(Math.random() * text.length), 1)[0];
+  b[4] = text.splice(Math.floor(Math.random() * text.length), 1)[0];
 
-  // switch (n) {
-  // 	case 0:
-  // 	  btn.textContent = "大吉";
-  // 	  break;
-  // 	case 1:
-  // 	  btn.textContent = "吉";
-  // 	  break;
-  // 	case 2:
-  // 	  btn.textContent = "凶";
-  // 	  break;
-  // }
-
-  });
+  console.log(b);
 }
